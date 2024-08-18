23284b38a894a7f7cc31f5a5f24aa4c2d6cea17b
andluca <trabalho.andre16@gmail.com>
2024-08-18 18:18:44 -0300

    feat: implementa rotas do projeto

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,8 @@
 import { createBrowserRouter, RouteObject } from "react-router-dom";
-import Home from "../pages/Home";
+import Home from "../pages/Home/Home";
+import Perfume from "../pages/Perfume/Perfume";
+import Lotion from "../pages/Lotion/Lotion";
+import Basket from "../pages/Basket/Basket";
 
 
 const routes: RouteObject[] = [
@@ -7,6 +10,18 @@ const routes: RouteObject[] = [
     path: "/",
     element: <Home />, 
   },
+  {
+    path: "/perfume",
+    element: <Perfume />,
+  },
+  {
+    path: "/lotion",
+    element: <Lotion />,
+  },
+  {
+    path: "/basket",
+    element: <Basket />,
+  }
 ];
 
 export const router = createBrowserRouter(routes);
