7b054b27ee9279f8cab4f9c221a4173dbb7efeab
andluca <trabalho.andre16@gmail.com>
2024-08-18 18:18:24 -0300

    feat: implementa páginas do projeto

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Header/>
+      <main>
+      
+      </main>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/pages/Lotion/Lotion.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+
+const Lotion: React.FC = () => {
+  return (
+    <>
+      <Header/>
+      <main>
+      
+      </main>
+    </>
+  );
+};
+
+export default Lotion;
