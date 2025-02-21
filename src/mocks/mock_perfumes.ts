cc3624c8d815494e8e1b4faa4f5431930e97be97
andvolveluca <179170638+andvolveluca@users.noreply.github.com>
2025-02-20 21:06:39 -0300

    feat: add mock data for perfumes with images, titles, descriptions, and prices

---
+++ src/mocks/mock_perfumes.ts
@@ -0,0 +1,146 @@
+import { CardProps } from "../components/Card/Card";
+import ladymillion from "../images/ladymillion.jpeg";
+import lavie from "../images/lavie.png";
+import scandal from "../images/scandal.png";
+import erbapura from "../images/erbapura.png";
+import dylanblue from "../images/dylanblue.png";
+import chloe from "../images/chloe.png";
+import two_one_two_rose from "../images/two_one_two_rose.png";
+import fame from "../images/fame.png";
+import goodgirl from "../images/goodgirl.png";
+import invictusvictory from "../images/invictusvictory.png";
+import fameblack from "../images/fameblack.png";
+import myparis from "../images/myparis.png";
+import laviecollect from "../images/laviecollect.png";
+import dylanturquoise from "../images/dylanturquoise.png";
+import heroine from "../images/heroine.png";
+import heroes from "../images/heroes.png";
+import jadore from "../images/jadore.png";
+import cocomademoiselle from "../images/cocomademoiselle.png";
+import sauvage from "../images/sauvage.png";
+import invictus from "../images/invictus.png";
+
+const perfumesMock: CardProps[] = [
+    {
+      imageUrl: ladymillion,
+      title: "Lady Million ",
+      description: "Brand Collection 105 Lady Million 25ml",
+      price: 78.90,
+    },
+    {
+      imageUrl: lavie,
+      title: "La Vie Est Belle",
+      description: "Brand Collection 012 La Vie Est Belle 25ml",
+      price: 78.90,
+    },
+    {
+      imageUrl: scandal,
+      title: "Scandal",
+      description: "Brand Collection 136 Scandal 25ml",
+      price: 78.90,
+    },
+    {
+      imageUrl: erbapura,
+      title: "Erba Pura Xerjoff",
+      description: "Brand Collection 402 Erba Pura Xerjoff 25ml",
+      price: 78.90,
+    },
+    {
+      imageUrl: dylanblue,
+      title: "Dylan Blue",
+      description: "Brand Collection 170 Dylan Blue 25ml",
+      price: 78.90,
+    },
+    {
+      imageUrl: chloe,
+      title: "Chloe",
+      description: "Brand Collection 177 Chloe 25ml",
+      price: 78.90,
+    },
+    {
+      imageUrl: two_one_two_rose,
+      title: "212 Vip Rose",
+      description: "Brand Collection 034 212 Vip Rose 25ml",
+      price: 78.90,
+    },
+    {
+      imageUrl: fame,
+      title: "Paco Rabanne Fame",
+      description: "Brand Collection 365 Fame Paco Rabanne 25ml",
+      price: 78.90,
+    },
+    {
+      imageUrl: goodgirl,
+      title: "Good Girl",
+      description: "Brand Collection 126 Good Girl 25ml",
+      price: 119.90,
+    },
+    {
+      imageUrl: invictusvictory,
+      title: "Invictus Victory",
+      description: "Brand Collection 214 Invictus Victory 25ml",
+      price: 78.90,
+    },
+    {
+      imageUrl: fameblack,
+      title: "Paco Rabbanne Fame Black",
+      description: "Brand Collection 415 Fame Paco Rabanne 25ml",
+      price: 78.90,
+    },
+    {
+      imageUrl: myparis,
+      title: "My Paris",
+      description: "Brand Collection 092 My Paris 25ml",
+      price: 78.90,
+    },
+    {
+      imageUrl: laviecollect,
+      title: "La Vie Est Belle Collection",
+      description: "Brand Collection 292 La Vie Est Belle 25ml",
+      price: 78.90,
+    },
+    {
+      imageUrl: dylanturquoise,
+      title: "Dylan Turquoise",
+      description: "Brand Collection 351 Dylan Turquoise 25ml",
+      price: 78.90,
+    },
+    {
+      imageUrl: heroine,
+      title: "212 Heroine",
+      description: "Brand Collection 340 212 Heroine 25ml",
+      price: 78.90,
+    },
+    {
+      imageUrl: heroes,
+      title: "212 Heroes",
+      description: "Brand Collection 347 212 Heroes 25ml",
+      price: 78.90,
+    },
+    {
+      imageUrl: jadore,
+      title: "J'adore",
+      description: "Brand Collection 007 J'adore 25ml",
+      price: 78.90,
+    },
+    {
+      imageUrl: cocomademoiselle,
+      title: "Coco Mademoiselle",
+      description: "Brand Collection 021 Coco Mademoiselle 25ml",
+      price: 78.90,
+    },
+    {
+      imageUrl: sauvage,
+      title: "Sauvage",
+      description: "Brand Collection 100 Sauvage 25ml",
+      price: 78.90,
+    },
+    {
+      imageUrl: invictus,
+      title: "Invictus",
+      description: "Brand Collection 116 Invictus 25ml",
+      price: 78.90,
+    }
+
+  ];
+export default perfumesMock;
