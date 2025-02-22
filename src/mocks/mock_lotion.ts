30231172743f0b5bac374cadd097836efde32c10
andvolveluca <andre.souza@advolve.ai>
2025-02-22 00:26:32 -0300

    feat: add multiple image files for perfumes and lotions

---
+++ src/mocks/mock_lotion.ts
@@ -0,0 +1,131 @@
+import { CardProps } from "../components/Card/Card";
+import jadore from "../images/lotions/jadore.png";
+import cocomademoiselle from "../images/lotions/cocomademoiselle.png";
+import nobreza from "../images/lotions/nobreza.webp";
+import prettywomen from "../images/lotions/prettywomen.webp";
+import armanicode from "../images/lotions/armanicode.webp";
+import beautygirl from "../images/lotions/beautygirl.webp";
+import lavie from "../images/lotions/lavie.webp";
+import heaven from "../images/lotions/heaven.webp";
+import livre from "../images/lotions/livre.webp";
+import lightdream from "../images/lotions/lightdream.webp";
+import sexylady from "../images/lotions/sexylady.webp";
+import savage from "../images/lotions/savage.webp";
+import cole from "../images/lotions/cole.webp";
+import jpclassic from "../images/lotions/jpclassic.webp";
+import onemillion from "../images/lotions/onemillion.webp";
+import euphoria from "../images/lotions/euphoria.webp";
+import escandal from "../images/lotions/escandal.webp";
+import ladymillion from "../images/lotions/ladymillion.webp";
+
+export const lotionsMock: CardProps[] = [
+    {
+        imageUrl: jadore,
+        title: "J'adore",
+        description: "Creme 007 J'adore 200ml",
+        price: 69.90,
+    },
+    {
+        imageUrl: cocomademoiselle,
+        title: "Coco Mademoiselle",
+        description: "Creme 021 Coco Mademoiselle 200ml",
+        price: 69.90,
+    },
+    {
+        imageUrl: nobreza,
+        title: "Nobreza",
+        description: "Creme 289 Nobreza 200ml",
+        price: 69.90,
+    },
+    {
+        imageUrl: prettywomen,
+        title: "CH",
+        description: "Crem 003 CH Carolina Herrera 200ml",
+        price: 69.90,
+    },
+    {
+        imageUrl: armanicode,
+        title: "Armani Code",
+        description: "Creme 012 Armani Code 200ml",
+        price: 69.90,
+    },
+    {
+        imageUrl: beautygirl,
+        title: "Beauty Girl",
+        description: "Creme 126 Beauty Girl 200ml",
+        price: 69.90,
+    },
+    {
+        imageUrl: lavie,
+        title: "La Vie Est Belle",
+        description: "Creme 012 La Vie Est Belle 200ml",
+        price: 69.90,
+    },
+    {
+        imageUrl: heaven,
+        title: "Heaven",
+        description: "Creme 168 Heaven 200ml",
+        price: 69.90,
+    },
+    {
+        imageUrl: livre,
+        title: "Livre",
+        description: "Creme 159 Livre 200ml",
+        price: 69.90,
+    },
+    {
+        imageUrl: lightdream,
+        title: "Light Dream",
+        description: "Creme 093 Light Dream 200ml",
+        price: 69.90,
+    },
+    {
+        imageUrl: sexylady,
+        title: "Sexy Lady",
+        description: "Creme 194 Sexy Lady 200ml",
+        price: 69.90,
+    },
+    {
+        imageUrl: savage,
+        title: "Savage",
+        description: "Creme 100 Savage 200ml",
+        price: 69.90,
+    },
+    {
+        imageUrl: cole,
+        title: "Cole",
+        description: "Creme 177 Cole 200ml",
+        price: 69.90,
+    },
+    {
+        imageUrl: jpclassic,
+        title: "JP Classic",
+        description: "Creme 171 JP Classic 200ml",
+        price: 69.90,
+    },
+    {
+        imageUrl: onemillion,
+        title: "One Million",
+        description: "Creme 005 One Million 200ml",
+        price: 69.90,
+    },
+    {
+        imageUrl: euphoria,
+        title: "Euphoria",
+        description: "Creme 097 Euphoria 200ml",
+        price: 69.90,
+    },
+    {
+        imageUrl: escandal,
+        title: "Scandal",
+        description: "Creme 136 Scandal 200ml",
+        price: 69.90,
+    },
+    {
+        imageUrl: ladymillion,
+        title: "Lady Million",
+        description: "Creme 105 Lady Million 200ml",
+        price: 69.90,
+    },
+
+]
--- src/mocks/mock_perfumes.ts
+++ src/mocks/mock_perfumes.ts
@@ -1,24 +1,39 @@
 import { CardProps } from "../components/Card/Card";
-import ladymillion from "../images/ladymillion.jpeg";
-import lavie from "../images/lavie.png";
-import scandal from "../images/scandal.png";
-import erbapura from "../images/erbapura.png";
-import dylanblue from "../images/dylanblue.png";
-import chloe from "../images/chloe.png";
-import two_one_two_rose from "../images/two_one_two_rose.png";
-import fame from "../images/fame.png";
-import goodgirl from "../images/goodgirl.png";
-import invictusvictory from "../images/invictusvictory.png";
-import fameblack from "../images/fameblack.png";
-import myparis from "../images/myparis.png";
-import laviecollect from "../images/laviecollect.png";
-import dylanturquoise from "../images/dylanturquoise.png";
-import heroine from "../images/heroine.png";
-import heroes from "../images/heroes.png";
-import jadore from "../images/jadore.png";
-import cocomademoiselle from "../images/cocomademoiselle.png";
-import sauvage from "../images/sauvage.png";
-import invictus from "../images/invictus.png";
+import ladymillion from "../images/parfums/ladymillion.jpeg";
+import lavie from "../images/parfums/lavie.png";
+import scandal from "../images/parfums/scandal.png";
+import erbapura from "../images/parfums/erbapura.png";
+import dylanblue from "../images/parfums/dylanblue.png";
+import chloe from "../images/parfums/chloe.png";
+import two_one_two_rose from "../images/parfums/two_one_two_rose.png";
+import fame from "../images/parfums/fame.png";
+import goodgirl from "../images/parfums/goodgirl.png";
+import invictusvictory from "../images/parfums/invictusvictory.png";
+import fameblack from "../images/parfums/fameblack.png";
+import myparis from "../images/parfums/myparis.png";
+import laviecollect from "../images/parfums/laviecollect.png";
+import dylanturquoise from "../images/parfums/dylanturquoise.png";
+import heroine from "../images/parfums/heroine.png";
+import heroes from "../images/parfums/heroes.png";
+import jadore from "../images/parfums/jadore.png";
+import cocomademoiselle from "../images/parfums/cocomademoiselle.png";
+import sauvage from "../images/parfums/sauvage.png";
+import invictus from "../images/parfums/invictus.png";
+import angel from "../images/parfums/angel.webp";
+import famepink from "../images/parfums/famepink.webp";
+import teddyboy from "../images/parfums/teddyboy.webp";
+import borninroma from "../images/parfums/bornroma.jpeg";
+import bluedream from "../images/parfums/bluedream.webp";
+import baccaratrouge from "../images/parfums/baccaratrouge.png";
+import dylanbluefemme from "../images/parfums/dylanbluefemme.webp";
+import olympea from "../images/parfums/olympea.webp";
+import alureG from "../images/parfums/alureG.webp";
+import allure from "../images/parfums/allure.webp";
+import idoleG from "../images/parfums/idoleG.jpg";
+import idole from "../images/parfums/idole.webp";
+import twoonetwoblackG from "../images/parfums/twoonetwoblackG.webp";
+import twoonetwoblack from "../images/parfums/twoonetwoblack.webp";
+import sauvageG from "../images/parfums/sauvageG.webp";
 
 const perfumesMock: CardProps[] = [
     {
@@ -130,6 +145,12 @@ const perfumesMock: CardProps[] = [
       price: 78.90,
     },
     {
+      imageUrl: sauvageG,
+      title: "Sauvage G",
+      description: "Brand Collection G-100 Sauvage 100ml",
+      price: 199.90,
+    },
+    {
       imageUrl: sauvage,
       title: "Sauvage",
       description: "Brand Collection 100 Sauvage 25ml",
@@ -139,6 +160,90 @@ const perfumesMock: CardProps[] = [
       imageUrl: invictus,
       title: "Invictus",
       description: "Brand Collection 116 Invictus 25ml",
+      price: 78.90,
+    },
+    {
+      imageUrl: angel,
+      title: "Alien",
+      description: "Brand Collection 043 Alien 25ml",
+      price: 78.90,
+    },
+    {
+      imageUrl: famepink,
+      title: "Paco Rabanne Fame Pink",
+      description: "Brand Collection 378 Fame Pink 25ml",
+      price: 78.90,
+    },
+    {
+      imageUrl: teddyboy,
+      title: "Teddy Boy",
+      description: "Brand Collection 394 Teddy Boy 25ml",
+      price: 78.90,
+    },
+    {
+      imageUrl: borninroma,
+      title: "Born In Roma",
+      description: "Brand Collection 391 Born In Roma 25ml",
+      price: 78.90,
+    },
+    {
+      imageUrl: bluedream,
+      title: "Blue Dream",
+      description: "Brand Collection 070 Blue Dream 25ml",
+      price: 78.90,
+    },
+    {
+      imageUrl: baccaratrouge,
+      title: "Baccarat Rouge",
+      description: "Brand Collection 380 Baccarat Rouge 25ml",
+      price: 78.90,
+    },
+    {
+      imageUrl: dylanbluefemme,
+      title: "Dylan Blue Femme",
+      description: "Brand Collection 265 Dylan Blue Femme 25ml",
+      price: 78.90,
+    },
+    {
+      imageUrl: olympea,
+      title: "Olympéa",
+      description: "Brand Collection 087 Olympéa 25ml",
+      price: 78.90,
+    },
+    {
+      imageUrl: alureG,
+      title: "Allure G",
+      description: "Brand Collection G-001 Allure 100ml",
+      price: 199.90,
+    },
+    {
+      imageUrl: allure,
+      title: "Allure",
+      description: "Brand Collection 001 Allure 25ml",
+      price: 78.90,
+    },	
+    {
+      imageUrl: idoleG,
+      title: "Idole G",
+      description: "Brand Collection G-238 Idole 80ml",
+      price: 179.90,
+    },
+    {
+      imageUrl: idole,
+      title: "Idole",
+      description: "Brand Collection 238 Idole 25ml",
+      price: 78.90,
+    },
+    {
+      imageUrl: twoonetwoblackG,
+      title: "212 Vip Black G",
+      description: "Brand Collection G-154 212 Vip Black 100ml",
+      price: 179.90,
+    },
+    {
+      imageUrl: twoonetwoblack,
+      title: "212 Vip Black",
+      description: "Brand Collection 154 212 Vip Black 25ml",
       price: 78.90,
     }
 
