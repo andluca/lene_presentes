71178e7b2c65031014e63a07304070d1b542352a
andvolveluca <andre.souza@advolve.ai>
2025-02-22 00:03:49 -0300

    feat: add WhatsApp link to Card component for purchasing perfumes

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -7,16 +7,23 @@ export interface CardProps {
   price: number;
 }
 
-
-
 const Card: React.FC<CardProps> = ({ imageUrl, title, description, price }) => {
+  const telefone = "5517992298165";
+  const url = `https://api.whatsapp.com/send?phone=${telefone}&text=Ol%C3%A1,%20gostaria%20de%20comprar%20os%20seguintes%20perfumes:%0D%0A-%20${encodeURIComponent(
+    title
+  )}`;
+
   return (
     <Styled.CardContainer>
       <Styled.CardImagePlaceholder src={imageUrl} />
-      <Styled.PriceContainer><span>R${price.toFixed(2)}</span></Styled.PriceContainer>
+      <Styled.PriceContainer>
+        <span>R${price.toFixed(2)}</span>
+      </Styled.PriceContainer>
       <Styled.CardTitle>{title}</Styled.CardTitle>
       <Styled.CardDescription>{description}</Styled.CardDescription>
-      <Styled.CardButton>Comprar</Styled.CardButton>
+      <Styled.LinkWrapper href={url} target="_blank">
+        <Styled.CardButton>Comprar</Styled.CardButton>
+      </Styled.LinkWrapper>
     </Styled.CardContainer>
   );
 };
