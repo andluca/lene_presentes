a79309c0b9fe7b9f07a5137b3a65605d320ea5c1
andvolveluca <109103844+andluca@users.noreply.github.com>
2025-02-27 21:26:43 -0300

    feat: add promotional banner to Card component and update props

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -6,9 +6,16 @@ export interface CardProps {
   title: string;
   description: string;
   price: number;
+  promotion?: boolean;
 }
 
-const Card: React.FC<CardProps> = ({ imageUrl, title, description, price }) => {
+const Card: React.FC<CardProps> = ({
+  imageUrl,
+  title,
+  description,
+  price,
+  promotion,
+}) => {
   const telefone = "5517992298165";
   const urlParfum = `https://api.whatsapp.com/send?phone=${telefone}&text=Ol%C3%A1,%20gostaria%20de%20comprar%20o%20${encodeURIComponent(
     title
@@ -19,10 +26,15 @@ const Card: React.FC<CardProps> = ({ imageUrl, title, description, price }) => {
 
   const location = useLocation();
   const currentPath = location.pathname;
-
   return (
     <Styled.CardContainer>
+      {promotion && (
+        <Styled.PromotionalBand>
+          <span>Promoção</span>
+        </Styled.PromotionalBand>
+      )}
       <Styled.CardImage loading="lazy" alt={title} src={imageUrl} />
+
       <Styled.PriceContainer>
         <span>R${price.toFixed(2)}</span>
       </Styled.PriceContainer>
