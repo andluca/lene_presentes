feef8604c1ef502a160d4b7e27e917efa1129a27
andvolveluca <andre.souza@advolve.ai>
2025-06-04 21:11:23 -0300

    feat: add createProduct function to handle product creation with form data

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,4 +1,4 @@
-import { ProductResponse } from "../models/Product";
+import { CreateProductDTO, ProductResponse } from "../models/Product";
 import api from "./api";
 
 export const listProducts = async (category?: string): Promise<ProductResponse[]> => {
@@ -6,4 +6,20 @@ export const listProducts = async (category?: string): Promise<ProductResponse[]
     params: category ? { category } : {},
   } );
   return  response.data;
+};
+
+export const createProduct = async (data: CreateProductDTO): Promise<ProductResponse> => {
+  const formData = new FormData();
+  formData.append("category", data.category);
+  formData.append("name", data.name);
+  formData.append("price", String(data.price));
+  formData.append("description", data.description);
+  formData.append("image", data.image);
+
+  const response = await api.post<ProductResponse>("/products/", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+  return response.data;
 };
